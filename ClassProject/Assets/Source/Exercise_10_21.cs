b4d42643285688768490b85fb38c6ebd62d0020d
B477042 <37619106+B477042@users.noreply.github.com>
2019-10-21 11:47:56 +0900

    2019 10 21 수업시간
    array와 가비지 컬렉션 설명, destory도

---
+++ ClassProject/Assets/Source/Exercise_10_21.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//만든날자 10월 21일
+//10*10 공간에 높이가 1~10사이의 bar를 만든다
+public class Exercise_10_21 : MonoBehaviour
+{
+    //3차원 배열을 선언하는 방법 => 1000개의 gameobject를 저장할 수 있는 공간을 만든다
+    private GameObject[,,] bars=new GameObject[10,10,10];
+    private float timer = 0.0f;
+    private void bulidBars()
+    {
+        //확인하기 위해서 y축으로 하나만 만들어본다
+        for(int x=0;x<10;x++)
+        {
+            for (int z = 0; z < 10; z++)
+            {
+                //int h = Random.Range(0, 10);
+                for(int y =0;y<10;y++)
+                {
+                    //이미 만들어져있다면 없애준다
+                    if(bars[x,y,z]!=null)
+                    {
+                        //game object를 삭제 시킨다
+                        Destroy(bars[x, y, z]);
+                        //null로 다시 초기화
+                        bars[x, y, z] = null;
+                    }
+
+
+                }
+                
+            }
+        }
+
+        for (int x = 0; x < 10; x++)
+        {
+            for (int z = 0; z < 10; z++)
+            {
+                int h = Random.Range(0, 10);
+                for (int y = 0; y < h; y++)
+                {
+                    var bar = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    bar.transform.position = new Vector3(x, y, z);
+                    bars[x, y, z] = bar;
+
+
+                }
+
+            }
+        }
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulidBars();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer>=3.0f)
+        {
+            bulidBars();
+            timer = 0.0f;
+        }
+    }
+}
--- ClassProject/Assets/Source/FPSAim.cs
+++ ClassProject/Assets/Source/FPSAim.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 //Attach to Camera Object
 //x->up,down, y=left,right, z=lean
+
+
+    //mouse 조작 프로그램
+    //만든시기 10월 16일, 마우스 입력
+
+    //21일에 하는건 키보드 입력
 public class FPSAim : MonoBehaviour
 {
     [SerializeField]
@@ -23,17 +29,81 @@ public class FPSAim : MonoBehaviour
         //=Debug.Log. 위치 확인
         // print("x : "+x+" y : "+y+", w : "+ Screen .width+", s : "+Screen.height );
 
-        //way2
-        //각 축의 마우스 이동량만큼을 반환시켜준다
-        float x = Input.GetAxis("Mouse X");
-        float y= Input.GetAxis("Mouse Y");
         //way1
         //y값을 가운데에 넣으면 y 축으로 회전을 해서 수평방향으로 움직이는걸로 보인다
         // transform.eulerAngles = new Vector3(-y*speed,x*speed,0);
 
 
-        //way2
-        //마우스의 움직인 양만큼만 움직이니 더해줘야된다
+        ////way2
+        ////각 축의 마우스 이동량만큼을 반환시켜준다
+        //float x = Input.GetAxis("Mouse X");
+        //float y= Input.GetAxis("Mouse Y");
+
+
+
+        ////way2
+        ////마우스의 움직인 양만큼만 움직이니 더해줘야된다
+        //transform.eulerAngles += new Vector3(-y * speed, x * speed, 0);
+
+        //way3 10.21
+        //keyboard
+        //getkeyway. 값으로 움직임의 속도를 조절하게 된다
+        //한번 누르면 땔 때까지 계속 입력이 주어지게 된다. frame단위로
+        //1초 누르면 FPS가 60이면 60번 입력된다. 누르고 있는 만큼 계속 입력된다
+
+        //    float x = 0.0f;
+        //    float y = 0.0f;
+        //    /* 
+        //     input api에서 입력을 처리해주는 것
+        //     여러 컴포넌트가 같은 프레임 안에선 같은 값을 가진다
+        //     */
+        //    if(Input.GetKey(KeyCode.W))
+        //    {
+        //        y += 1;
+        //    }
+        //    if (Input.GetKey(KeyCode.S))
+        //    {
+        //        y -= 1;
+        //    }
+        //    if (Input.GetKey(KeyCode.A))
+        //    {
+        //        x -= 1;
+        //    }
+        //    if (Input.GetKey(KeyCode.D))
+        //    {
+        //        x += 1;
+        //    }
+
+        //    transform.eulerAngles += new Vector3(-y * speed, x * speed, 0);
+        //}
+
+
+
+        //way 4 getkey down
+        float x = 0.0f;
+        float y = 0.0f;
+        /* 
+         누르기 시작한 시점에서 호출이 된다.
+         한번 누르면 한번 움직이니 연타해야 계속 움직인다
+         
+         */
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            y += 1;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            y -= 1;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            x -= 1;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            x += 1;
+        }
+
         transform.eulerAngles += new Vector3(-y * speed, x * speed, 0);
     }
 }
