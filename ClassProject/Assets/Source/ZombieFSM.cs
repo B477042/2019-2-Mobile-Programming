b0f74c74b5cc32bef5f5633a404910acff483bb3
B477042 <37619106+B477042@users.noreply.github.com>
2019-11-05 16:45:30 +0900

    class code fix

--- ClassProject/Assets/Source/ZombieFSM.cs
+++ ClassProject/Assets/Source/ZombieFSM.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 public enum ZombieState
 {
     Idle,
-    Wondering,
-    Tracking,
+    Wandering,
+    Tracing,
     Attacking
 }
 
@@ -14,11 +14,14 @@ public class ZombieFSM : MonoBehaviour
     private ZombieState state = ZombieState.Idle;
     private Renderer renderer;
     private GameObject player;
+    [SerializeField]
+    float speed = 0.1f;
 
    private const float visibleRange = 10.0f;
     private const float attackRange = 5.0f;
     private float timer = 0.0f;
-    private float nextActionTimer = 3.0f;
+    private float nextActionTimer = 2.0f;
+    private Vector3 targetPos;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,24 +32,53 @@ public class ZombieFSM : MonoBehaviour
     {
         return Vector3.Distance(transform.position, player.transform.position);
 ;    }
+
+    
+
     // Update is called once per frame
     void Update()
     {
         float dist = calcDistanceToPlayer();
-        timer += Time.deltaTime;
-        if (timer < nextActionTimer) return;
+        //timer += Time.deltaTime;
+        //if (timer < nextActionTimer) return;
+        
         switch(state)
         {
             case ZombieState.Idle:
-                
+                timer += Time.deltaTime;
                 renderer.material.color=Color.white;
+                if(timer>3.0f)
+                {
+                    timer = 0.0f;
+                    state = ZombieState.Wandering;
+                    targetPos = transform.position;
+                }
                 break;
-            case ZombieState.Wondering:
-              
+            case ZombieState.Wandering:
+                timer += Time.deltaTime;
+                
+                
+                if(timer>7.0f)
+                {
+                    targetPos = transform.position;
+                    timer = 0.0f;
+                    state = ZombieState.Idle;
+                    break;
+
+                }
+                if(transform.position==targetPos)
+                {
+                    targetPos += new Vector3(Random.Range(-3.0f, 3.0f), 0.0f, Random.Range(-3.0f, 3.0f));
+
+                }
+
+                transform.position = Vector3.MoveTowards(transform.position, targetPos, speed);
                 renderer.material.color = Color.green;
+
                 break;
-            case ZombieState.Tracking:
-              
+            case ZombieState.Tracing:
+                targetPos = player.transform.position;
+              transform.position+=  Vector3.MoveTowards(transform.position, targetPos, 0.1f);
                 renderer.material.color = Color.yellow;
                 break;
             case ZombieState.Attacking:
@@ -60,12 +92,22 @@ public class ZombieFSM : MonoBehaviour
             if (dist < attackRange)
                 state = ZombieState.Attacking;
             else
-                state = ZombieState.Tracking;
+                state = ZombieState.Tracing;
         }
         else
-            state = ZombieState.Idle;
+        {
+            switch (state)
+            {
+                case ZombieState.Idle:
+                case ZombieState.Wandering:
+                    break;
+                case ZombieState.Attacking:
+                case ZombieState.Tracing:
+                    state = ZombieState.Idle;
+                    break;
+            }
+        }
 
-        timer = 0.0f;
     }
-    
+
 }
