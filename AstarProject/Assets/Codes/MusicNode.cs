0efefb39ad7f9d51188bd90addf541c10a278d6b
B477042 <37619106+B477042@users.noreply.github.com>
2019-12-14 10:10:30 +0900

    446

--- AstarProject/Assets/Codes/MusicNode.cs
+++ AstarProject/Assets/Codes/MusicNode.cs
@@ -2,16 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicBlock : MonoBehaviour
+//음악 노드
+//하늘에서 내려오는 그것
+public class MusicNode : MonoBehaviour
 {
+  private  Renderer renderer;
     // Start is called before the first frame update
     void Start()
     {
+        renderer = GetComponent<Renderer>();
         
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+    public void ChangeColor(Color newColor)
+    {
+        renderer.material.color = newColor;
+    }
+    public Vector3 GetPos()
+    {
+        return transform.position;
+    }
+    public void PopNode()
+    {
+        Destroy(this);
+    }
+    public void BeingPossed(MusicNodeController nodeController)
     {
         
     }
--- AstarProject/Assets/Codes/MusicNodeController.cs
+++ AstarProject/Assets/Codes/MusicNodeController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicBlockController : MonoBehaviour
+public class MusicNodeController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
